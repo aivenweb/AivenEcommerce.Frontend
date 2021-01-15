d53f27b21904612b26a7522beab75454d731ce2e
nazarenoventrelli <40725187+nazarenoventrelli@users.noreply.github.com>
2021-01-15 19:05:51 -0300

    files moved to root

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { CheckoutComponent } from './checkout/checkout.component';
+import { CartComponent } from './cart/cart.component';
+import { ProductDetailComponent } from './product-detail/product-detail.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    CheckoutComponent,
+    CartComponent,
+    ProductDetailComponent
   ],
   imports: [
     BrowserModule,
